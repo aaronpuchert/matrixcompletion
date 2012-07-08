56c9199c710d48744c88dd537c4a15f8d634ea68
Aaron Puchert <aaronpuchert@alice-dsl.net>
2012-07-08 21:20:15 +0200

    Corrected many mistakes.

--- data.r
+++ data.r
@@ -3,19 +3,22 @@
 read.data <- function (filename, ...)
 {
 	tab <- read.table(filename, sep=",", header=FALSE, ...);
-	if (dim(tab)[2] == 2) col.names(tab) = c("user", "movie") else col.names(tab) = c("user", "movie", "stars");
+	if (dim(tab)[2] == 2) colnames(tab) <- c("user", "movie") else colnames(tab) <- c("user", "movie", "stars");
 	class(tab) <- c("data", "data.frame");
 	tab
 }
 
 # reduce user and movie domain
-reduce.data <- function (df, feat=-length(df))
+reduce.data <- function (df, ui = sort(union(df$user, NULL)), mi = sort(union(df$movie, NULL)))
 {
-	ui <- sort(union(df$user, NULL));
-	mi <- sort(union(df$movie, NULL));
 	df$user <- sapply(df$user, function(x) {which(ui==x)});
 	df$movie <- sapply(df$movie, function(x) {which(mi==x)});
 	list(data=df,ui=ui,mi=mi)
+}
+
+expand.data <- function (reduced, ui, mi)
+{
+	data.frame(user=ui[reduced$user], movie=mi[reduced$movie], reduced[-(1:2)]);
 }
 
 # plot in rainbow colors
@@ -32,31 +35,32 @@ matrix.data <- function (df, init=0)
 
 ### EVALUATION & OUTPUT ###
 # evaluate matrix on test data set
-eval <- function(df.test, mat) {
-	cbind(df.test, est=0);
-	apply(df.test, 1, function(x) {x$est <- mat[x$user,x$movie];})
+eval <- function(df.test, mat)
+{
+	new <- apply(df.test, 1, function(x) {c(x, est=mat[x[[1]],x[[2]]]);});
+	as.data.frame(t(new))
 }
 
 # write to result file
-write.data <- function(df.test, filename, ...) {write.table(df.test, filename, sep=",", col.names=FALSE, row.names=FALSE, ...}
+write.data <- function(df.test, filename, ...) {write.table(df.test, filename, sep=",", col.names=FALSE, row.names=FALSE, ...)}
 
 # compute RMSE
 error.data <- function(df.test) {sqrt(mean((df.test$stars - df.test$est)^2))}
 
 # cross-validation
-crossval <- function(df, k, alg, ...)
+crossval <- function(df, m, alg, ...)
 {
 	err <- 0; len <- length(df[[1]]);
-	for (i in 1:k) {
-		// split data set
-		df.test <- df[((i-1)*len/k):(i*len/k),];
-		df.learn <- df[-(((i-1)*len/k):(i*len/k)),];
-		// learn
+	for (i in 1:m) {
+		# split data set
+		df.test <- df[((i-1)*len/m):(i*len/m),];
+		df.learn <- df[-(((i-1)*len/m):(i*len/m)),];
+		# learn
 		mat <- alg(df.learn, ...);
-		// estimate on test data
+		# estimate on test data
 		df.test <- eval(df.test, mat);
-		// compute RMSE
+		# compute RMSE
 		err <- err + error.data(df.test)^2
 	}
-	sqrt(err/k)
+	sqrt(err/m)
 }
