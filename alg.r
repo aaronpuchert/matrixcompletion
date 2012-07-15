6c28146bae713b9808deb93c50b8cd10cf42d21e
Aaron Puchert <aaronpuchert@alice-dsl.net>
2012-07-16 01:48:39 +0200

    First sketch of Hazan's algorithm.

--- alg.r
+++ alg.r
@@ -9,7 +9,7 @@ alg.svd <- function(df, init=mean(df$stars), k=1, eps=0.01)
 	errvec <- max(df$stars) - min(df$stars);
 	errvec <- c(errvec, errvec/(1-eps)^2);
 	while (errvec[1]/errvec[2] < 1-eps) {
-		errvec <- c(sqrt(sum((mat*mask-given)^2) / sum(mask)), errvec);
+		errvec <- c(sqrt(sum((mat*mask-given)^2) / nrow(df)), errvec);
 		mat <- mat*(1-mask)+given;
 		sing <- La.svd(mat, k, k);
 		mat <- sing$u[,1:k] %*% diag(sing$d[1:k],k) %*% sing$v[1:k,]
@@ -17,6 +17,29 @@ alg.svd <- function(df, init=mean(df$stars), k=1, eps=0.01)
 
 	print(errvec);
 	return(mat)
+}
+
+# Hazan's algorithm
+alg.hazan <- function(df, t=1, eps=0.01)
+{
+	n <- max(df$user); m <- max(df$movie); l <- nrow(df);
+	Y <- matrix(NA, n+m, n+m);
+	apply(df, 1, function(x) {Y[x[[1]]+m, x[[2]]] <<- x[[3]]} );
+
+	errvec <- max(df$stars) - min(df$stars);	# how to initialize?
+	errvec <- c(errvec, errvec/(1-eps)^2);
+	i <- 0; v <- runif(n+m);
+	X <- t * v %*% t(v);
+	while (errvec[1]/errvec[2] < 1-eps) {
+		errvec <- c(sum(ifelse(Y!=NA, (X-Y)^2, 0))/l, errvec);
+		alpha <- 2/(i+2);	i <- i+1;
+		Nabla <- ifelse(Y!=NA, 2*(X-Y), 0);
+		eig <- eigen(Nabla, symmetric=TRUE);
+		X <- (1-alpha)*X + alpha*t*eig$vectors[1,] %*% t(eig$vectors[1,]);
+	};
+
+	print(errvec);
+	return(X[(m+1):(n+m),1:m])
 }
 
 # some post processing
