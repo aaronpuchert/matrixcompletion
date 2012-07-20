48d6699196bd03a9f058b9f5ca14690a6a3ab12e
Aaron Puchert <aaron.puchert@uni-jena.de>
2012-07-20 16:08:30 +0200

    Removed unnecessary delimiters. (especially ';')

--- alg.r
+++ alg.r
@@ -1,21 +1,21 @@
 # SVD algorithm with rank k approximation, stops when error decrease is below eps.
 alg.svd <- function(df, pl=alg.svd.pl(df))
 {
-	mat <- matrix(pl$init, max(df$user), max(df$movie));
-	given <- mask <- matrix(0, max(df$user), max(df$movie));
-	given[(df$movie-1)*nrow(mat)+df$user] <- df$stars;		# matrix of given values
-	mask[(df$movie-1)*nrow(mat)+df$user] <- 1;				# contains a '1' for each given value
+	mat <- matrix(pl$init, max(df$user), max(df$movie))
+	given <- mask <- matrix(0, max(df$user), max(df$movie))
+	given[(df$movie-1)*nrow(mat)+df$user] <- df$stars		# matrix of given values
+	mask[(df$movie-1)*nrow(mat)+df$user] <- 1				# contains a '1' for each given value
 
 	# error history, initialize with dummy values making no trouble.
-	errvec <- (max(df$stars) - min(df$stars)) * c(1, 1/(1-pl$eps)^2);
+	errvec <- (max(df$stars) - min(df$stars)) * c(1, 1/(1-pl$eps)^2)
 	while (errvec[1]/errvec[2] < 1-pl$eps) {
-		errvec <- c(sqrt(sum((mat*mask-given)^2) / nrow(df)), errvec); # compute error
-		mat <- mat*(1-mask)+given;										# then overwrite with given values 
-		sing <- La.svd(mat, pl$k, pl$k);
+		errvec <- c(sqrt(sum((mat*mask-given)^2) / nrow(df)), errvec) # compute error
+		mat <- mat*(1-mask)+given										# then overwrite with given values 
+		sing <- La.svd(mat, pl$k, pl$k)
 		mat <- sing$u %*% diag(sing$d[1:pl$k],pl$k) %*% sing$v			# and compute rank k approximation
-	};
+	}
 
-	print(errvec);
+	print(errvec)
 	return(mat)
 }
 
@@ -25,35 +25,35 @@ alg.svd.pl <- function(df, init=mean(df$stars), k=1, digits=2) list(init=init, k
 #	averaged on error history of maximum length maxhist.
 alg.hazan <- function(df, pl=alg.hazan.pl(df, mean(df$stars)*(max(df$user) + max(df$movie))))
 {
-	n <- max(df$user); m <- max(df$movie); len <- nrow(df);
-	Y <- matrix(0, n+m, n+m);
-	Y[(df$movie-1)*(n+m)+df$user+m] <- df$stars;	# "given" matrix Y we want to approximate 
-	Y[(df$user-1+m)*(n+m)+df$movie] <- df$stars;
+	n <- max(df$user); m <- max(df$movie); len <- nrow(df)
+	Y <- matrix(0, n+m, n+m)
+	Y[(df$movie-1)*(n+m)+df$user+m] <- df$stars	# "given" matrix Y we want to approximate 
+	Y[(df$user-1+m)*(n+m)+df$movie] <- df$stars
 
 	# initialize X = v*v^T with an eigenvector belonging to the greatest eigenvalue 
-	i <- 0; v <- power.method(Y, pl$Cf/pl$tr);
-	X <- pl$tr * (v %*% t(v)) / sum(v*v);
-	errvec <- (sum(ifelse(Y!=0, (X-Y)^2, 0))/len) * c(1/(1-pl$eps)^2, 1/(1-pl$eps)^4);
+	i <- 0; v <- power.method(Y, pl$Cf/pl$tr)
+	X <- pl$tr * (v %*% t(v)) / sum(v*v)
+	errvec <- (sum(ifelse(Y!=0, (X-Y)^2, 0))/len) * c(1/(1-pl$eps)^2, 1/(1-pl$eps)^4)
 
 	decr <- 1;	# average error decrease
 	while (decr > pl$eps | i < 10) {
 		# compute error and average error decrease
-		errvec <- c(sum(ifelse(Y!=0, (X-Y)^2, 0))/len, errvec);
-		hist <- min(pl$maxhist, length(errvec));
+		errvec <- c(sum(ifelse(Y!=0, (X-Y)^2, 0))/len, errvec)
+		hist <- min(pl$maxhist, length(errvec))
 		decr <- lm(errvec[1:hist] ~ as.numeric(1:hist))$coefficients[[2]]
 
-		alpha <- 2/(i+2);	i <- i+1;
+		alpha <- 2/(i+2);	i <- i+1
 		# compute "symmetricized" gradient matrix of f(X) = \sum_(\Omega+(0,m)) (X_ij-Y_ij)^2
-		Nabla <- ifelse(Y!=0, 2*(X-Y), 0);
+		Nabla <- ifelse(Y!=0, 2*(X-Y), 0)
 
 		# compute an eigenvector corresponding to the greatest eigenvalue
-		v <- power.method(Nabla, alpha*pl$Cf/pl$tr);
+		v <- power.method(Nabla, alpha*pl$Cf/pl$tr)
 
 		# blend old X with tr*v*v^T
-		X <- (1-alpha)*X + alpha*pl$tr * v %*% t(v);
-	};
+		X <- (1-alpha)*X + alpha*pl$tr * v %*% t(v)
+	}
 
-	print(errvec);
+	print(errvec)
 	return(X[(m+1):(n+m),1:m])
 }
 
@@ -62,12 +62,12 @@ alg.hazan.pl <- function(df, tr, digits=2, Cf=curvature(df[c(1,2)]), maxhist=5) 
 # "power method" to compute an eigenvector corresponding to the greatest eigenvalue
 power.method <- function(A, eps)
 {
-	v <- runif(dim(A)[1]); v <- v/sqrt(sum(v*v));
+	v <- runif(dim(A)[1]); v <- v/sqrt(sum(v*v))
 	l<-2; oldl<-1;	# not "correct", but works in high dimensions
 	while (l/oldl > 1 + eps) {
-		v <- A %*% v;
-		oldl <- l; l <- sqrt(sum(v*v));
-		v <- v/l;
+		v <- A %*% v
+		oldl <- l; l <- sqrt(sum(v*v))
+		v <- v/l
 	}
 	return(v)
 }
@@ -75,21 +75,21 @@ power.method <- function(A, eps)
 # compute lower bound for the "curvature constant" C_f
 curvature <- function(df, samples=10)
 {
-	n <- max(df$user); m <- max(df$movie); len <- nrow(df);
-	Y <- matrix(0, n+m, n+m);
-	Y[(df$movie-1)*nrow(Y)+df$user+m] <- 1;
+	n <- max(df$user); m <- max(df$movie); len <- nrow(df)
+	Y <- matrix(0, n+m, n+m)
+	Y[(df$movie-1)*nrow(Y)+df$user+m] <- 1
 
-	samp <- vector(mode="list", length=samples);
+	samp <- vector(mode="list", length=samples)
 	for (i in 1:samples) {
-		A <- matrix(rnorm((n+m)^2), nrow=n+m);		# generate n independent vectors
+		A <- matrix(rnorm((n+m)^2), nrow=n+m)		# generate n independent vectors
 		A <- apply(A, 2,
-			function(x) {x/sqrt(sum(x*x))});		# normalize them
-		w <- rexp(n+m, n+m); w <- w / sum(w);		# generate weights
-		samp[[i]] <- A %*% (t(A)*w);				# generate matrix
+			function(x) {x/sqrt(sum(x*x))})		# normalize them
+		w <- rexp(n+m, n+m); w <- w / sum(w)		# generate weights
+		samp[[i]] <- A %*% (t(A)*w)					# generate matrix
 	};
 
 	maximum <- 0;
-	for (i in 1:samples) for (j in 1:samples) { diff<-sum((samp[[i]]-samp[[j]])^2 * Y)/len; maximum <- max(maximum, diff)};
+	for (i in 1:samples) for (j in 1:samples) { diff<-sum((samp[[i]]-samp[[j]])^2 * Y)/len; maximum <- max(maximum, diff) }
 	return(maximum)
 }
 
@@ -98,7 +98,7 @@ post.clamp <- function(data, rg) { ifelse(data<rg[1], rg[1], ifelse(data>rg[2], 
 post.linear <- function(data, rg) { old<-range(data); (data-old[1])/(old[2]-old[1])*(rg[2]-rg[1])+rg[1] }
 post.logistic <- function(data, rg)
 {
-	data <- (2*data-(rg[1]+rg[2]))/(rg[2]-rg[1]);	# scale to [-1,1]
-	data <- 1/(1+exp(-2*data));						# apply logistic function with derivative 1/2 in 0.
+	data <- (2*data-(rg[1]+rg[2]))/(rg[2]-rg[1])	# scale to [-1,1]
+	data <- 1/(1+exp(-2*data))						# apply logistic function with derivative 1/2 in 0.
 	data*(rg[2]-rg[1]) + rg[1]
 }
--- data.r
+++ data.r
@@ -2,23 +2,23 @@
 # read csv data
 read.data <- function (filename, ...)
 {
-	tab <- read.table(filename, sep=",", header=FALSE, ...);
-	if (ncol(tab) == 2) colnames(tab) <- c("user", "movie") else colnames(tab) <- c("user", "movie", "stars");
-	class(tab) <- c("data", "data.frame");
+	tab <- read.table(filename, sep=",", header=FALSE, ...)
+	if (ncol(tab) == 2) colnames(tab) <- c("user", "movie") else colnames(tab) <- c("user", "movie", "stars")
+	class(tab) <- c("data", "data.frame")
 	tab
 }
 
 # reduce user and movie domain
 reduce.data <- function (df, ui = sort(union(df$user, NULL)), mi = sort(union(df$movie, NULL)))
 {
-	df$user <- sapply(df$user, function(x) {which(ui==x)});
-	df$movie <- sapply(df$movie, function(x) {which(mi==x)});
+	df$user <- sapply(df$user, function(x) {which(ui==x)})
+	df$movie <- sapply(df$movie, function(x) {which(mi==x)})
 	list(data=df,ui=ui,mi=mi)
 }
 
 expand.data <- function (reduced, ui, mi)
 {
-	data.frame(user=ui[reduced$user], movie=mi[reduced$movie], reduced[-(1:2)]);
+	data.frame(user=ui[reduced$user], movie=mi[reduced$movie], reduced[-(1:2)])
 }
 
 # plot in rainbow colors
@@ -28,8 +28,8 @@ plot.data <- function(df, rg=c(1,5), col=3, ...)
 # create data matrix
 matrix.data <- function (df, init=NA)
 {
-	mat <- matrix(init, max(df[[1]]), max(df[[2]]));
-	mat[(df$movie-1)*nrow(mat)+df$user] <- df$stars;
+	mat <- matrix(init, max(df[[1]]), max(df[[2]]))
+	mat[(df$movie-1)*nrow(mat)+df$user] <- df$stars
 	mat
 }
 
@@ -37,12 +37,12 @@ matrix.data <- function (df, init=NA)
 # evaluate matrix on test data set
 eval <- function(df, mat)
 {
-	df <- cbind(df, est=NA);
+	df <- cbind(df, est=NA)
 	df$est <- mat[(df$movie-1)*nrow(mat)+df$user]; df
 }
 
 # write to result file
-write.data <- function(df.test, filename, ...) {write.table(df.test, filename, sep=",", col.names=FALSE, row.names=FALSE, ...)}
+write.data <- function(df.test, filename, ...) write.table(df.test, filename, sep=",", col.names=FALSE, row.names=FALSE, ...)
 
 # compute error (& analyze it)
 error.data <- function(df.test)
@@ -54,15 +54,15 @@ error.data <- function(df.test)
 # cross-validation
 crossval <- function(df, m, alg, params)
 {
-	len <- nrow(df); df <- cbind(df, est=NA);
-	perm <- sample(len);		# permute randomly to gain fair results
+	len <- nrow(df); df <- cbind(df, est=NA)
+	perm <- sample(len)		# permute randomly to gain fair results
 	for (i in 1:m) {
 		# select test data
-		ind <- perm[ceiling((i-1)*len/m):floor(i*len/m)];
+		ind <- perm[ceiling((i-1)*len/m):floor(i*len/m)]
 		# learn
-		mat <- alg(df[-ind,], params);
+		mat <- alg(df[-ind,], params)
 		# estimate on test data
-		df$est[ind] <- mat[(df$movie[ind]-1)*nrow(mat) + df$user[ind]];
+		df$est[ind] <- mat[(df$movie[ind]-1)*nrow(mat) + df$user[ind]]
 	}
 
 	# return cross-validated data set
@@ -74,38 +74,38 @@ bisect <- function(df, cvm, alg, params, target, rg, numit=20, int=FALSE)
 {
 	gen.bisect(
 		function(x) {
-				params[[target]] <- ifelse(int, round(x), x);
-				res <- crossval(df, cvm, alg, params);
-				error <- error.data(res);
+				params[[target]] <- ifelse(int, round(x), x)
+				res <- crossval(df, cvm, alg, params)
+				error <- error.data(res)
 				print(error);	error$error
 			},
-		rg[1], rg[2], numit);
+		rg[1], rg[2], numit)
 }
 
 # find the minimum of a convex (not necessarily diff'able) function, assuming the given interval contains it
 gen.bisect <- function(fun, left, right, numit)
 {
-	middle <- runif(1, left, right);
-	fmiddle <- fun(middle);
+	middle <- runif(1, left, right)
+	fmiddle <- fun(middle)
 
 	for (i in 1:numit) {
-		new <- runif(1, left, right);
-		fnew <- fun(new);
+		new <- runif(1, left, right)
+		fnew <- fun(new)
 		if (new < middle) {			# "new" left of "middle"
 			if (fnew < fmiddle) {
-				right <- middle;
-				middle <- new;
-				fmiddle <- fnew;
+				right <- middle
+				middle <- new
+				fmiddle <- fnew
 			}
-			else {left <- new;}
+			else {left <- new}
 		}
 		else {						# "new" right of "middle"
 			if (fnew < fmiddle) {
-				left <- middle;
-				middle <- new;
-				fmiddle <- fnew;
+				left <- middle
+				middle <- new
+				fmiddle <- fnew
 			}
-			else {right <- new;}
+			else {right <- new}
 		}
 	}
 
@@ -115,16 +115,16 @@ gen.bisect <- function(fun, left, right, numit)
 ### META-ROUTINES ###
 read.all <- function(trainfile, qualfile)
 {
-	train <- read.data(trainfile);
-	newtrain <- reduce.data(train);
-	qual <- read.data(qualfile);
-	newqual <- reduce.data(qual, newtrain$ui, newtrain$mi);
+	train <- read.data(trainfile)
+	newtrain <- reduce.data(train)
+	qual <- read.data(qualfile)
+	newqual <- reduce.data(qual, newtrain$ui, newtrain$mi)
 	list(train=newtrain$data, qual=newqual$data, ui=newtrain$ui, mi=newtrain$mi)
 }
 
 write.res <- function(all, mat, resfile)
 {
-	qual <- eval(all$qual, mat);
-	qual <- expand.data(qual, all$ui, all$mi);
-	write.data(qual, resfile);
+	qual <- eval(all$qual, mat)
+	qual <- expand.data(qual, all$ui, all$mi)
+	write.data(qual, resfile)
 }
